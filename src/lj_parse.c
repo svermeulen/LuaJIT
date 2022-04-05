03d73ff9d4c1be58804dc7e1a42155148886b970
Steve Vermeulen <sfvermeulen@gmail.com>
2022-04-05 16:15:54 -0700

    Increase max path length for paths displayed in error message stack traces. Also disabled tail calls to make stack traces more complete

--- src/lj_parse.c
+++ src/lj_parse.c
@@ -136,7 +136,7 @@ typedef struct FuncState {
   BCInsLine *bcbase;		/* Base of bytecode stack. */
   BCPos bclim;			/* Limit of bytecode stack. */
   MSize vbase;			/* Base of variable stack for this function. */
-  uint8_t flags;		/* Prototype flags. */
+  uint32_t flags;		/* Prototype flags. */
   uint8_t numparams;		/* Number of parameters. */
   uint8_t framesize;		/* Fixed frame size. */
   uint8_t nuv;			/* Number of upvalues */
@@ -1629,7 +1629,7 @@ static void fs_init(LexState *ls, FuncState *fs)
   fs->nactvar = 0;
   fs->nuv = 0;
   fs->bl = NULL;
-  fs->flags = 0;
+  fs->flags = PROTO_NO_TAILCALL;
   fs->framesize = 1;  /* Minimum frame size. */
   fs->kt = lj_tab_new(L, 0, 0);
   /* Anchor table of constants in stack to avoid being collected. */
@@ -2337,7 +2337,7 @@ static void parse_return(LexState *ls)
       if (e.k == VCALL) {  /* Check for tail call. */
 	BCIns *ip = bcptr(fs, &e);
 	/* It doesn't pay off to add BC_VARGT just for 'return ...'. */
-	if (bc_op(*ip) == BC_VARG) goto notailcall;
+	if ((fs->flags & PROTO_NO_TAILCALL) || bc_op(*ip) == BC_VARG) goto notailcall;
 	fs->pc--;
 	ins = BCINS_AD(bc_op(*ip)-BC_CALL+BC_CALLT, bc_a(*ip), bc_c(*ip));
       } else {  /* Can return the result from any register. */
